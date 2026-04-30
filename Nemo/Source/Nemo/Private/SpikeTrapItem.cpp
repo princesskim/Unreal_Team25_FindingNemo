76d87859e1ffd5593880af824503136a100e0de4
TsukinoRen <whitealbum1226@gmail.com>
2026-04-30 10:50:33 +0900

    <장원재> [UPDATE] SpikeTrapItem, BombItem / BaseCreature 데미지 연동 및 디버그 로그 추가
       - 기존 Pawn 연동 기준 로직에서 BaseCreature 기준으로 로직 변경
       - 충돌 테스트용 디버그 추가

--- Nemo/Source/Nemo/Private/SpikeTrapItem.cpp
+++ Nemo/Source/Nemo/Private/SpikeTrapItem.cpp
@@ -1,30 +1,60 @@
-#include "SpikeTrapItem.h"
+﻿#include "SpikeTrapItem.h"
 #include "Components/SphereComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "BaseCreature.h"
 
 ASpikeTrapItem::ASpikeTrapItem()
 {
-	SpikeDamage = 10.0f;
-
-	ItemType = "Spkie";
+    SpikeDamage = 10;
+    DamageInterval = 0.5f;
+    CurrentActivator = nullptr;
+    ItemType = "Spike";
 }
 
+// 밟는 순간 CurrentActivator 저장 후 반복 타이머 시작
 void ASpikeTrapItem::ActivateItem(AActor* Activator)
 {
-	TArray<AActor*> OverlappingActors;
-	SpikeCollision->GetOverlappingActors(OverlappingActors);
+    GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("[SpikeTrap] 밟힘 / 타이머 시작"));
 
-	for (AActor* Actor : OverlappingActors)
-	{
-		if (Actor && Actor->ActorHasTag("Player"))
-		{
-			UGameplayStatics::ApplyDamage(
-				Actor,
-				SpikeDamage,
-				nullptr,
-				this,
-				UDamageType::StaticClass()
-			);
-		}
-	}
+    CurrentActivator = Activator;
+    GetWorld()->GetTimerManager().SetTimer(
+        SpikeDamageTimerHandle,
+        this,
+        &ASpikeTrapItem::ApplySpikeDamage,
+        DamageInterval,
+        true
+    );
+}
+
+// CurrentActivator에게 SpikeDamage 적용
+void ASpikeTrapItem::ApplySpikeDamage()
+{
+    if (CurrentActivator)
+    {
+        // UGameplayStatics::ApplyDamage 대신
+        ABaseCreature* Creature = Cast<ABaseCreature>(CurrentActivator);
+        if (Creature)
+        {
+            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, FString::Printf(TEXT("[SpikeTrap] 데미지 적용: %d"), SpikeDamage));
+
+            Creature->ApplyDamage(SpikeDamage, this);
+        }
+    }
+}
+
+// 타이머에서 반복 호출되는 데미지 처리 함수
+// 밟았던 액터가 벗어나면 타이머 정지 및 CurrentActivator 초기화
+void ASpikeTrapItem::OnItemEndOverlap(
+    UPrimitiveComponent* OverlappedComp,
+    AActor* OtherActor,
+    UPrimitiveComponent* OtherComp,
+    int32 OtherBodyIndex)
+{
+    if (OtherActor == CurrentActivator)
+    {
+        GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("[SpikeTrap] 벗어남 / 타이머 정지"));
+
+        GetWorld()->GetTimerManager().ClearTimer(SpikeDamageTimerHandle);
+        CurrentActivator = nullptr;
+    }
 }
